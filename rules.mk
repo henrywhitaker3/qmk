f1ffee9128fd3a73eaf75719d42bce7f782d3d89
Henry Whitaker <henrywhitaker3@outlook.com>
2025-03-04 12:00:25 +0000

    fix: correct bootloader

--- rules.mk
+++ rules.mk
@@ -1,4 +1,4 @@
-# BOOTLOADER = atmel-dfu
+BOOTLOADER = rp2040
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes        # Audio control and System control
